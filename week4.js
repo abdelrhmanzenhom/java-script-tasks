95a209f04b2d56da8e99de572cc78ab93d746864
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-09-08 21:54:01 +0300

    week4

---
+++ week4.js
@@ -0,0 +1,48 @@
+/* let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar)); // 100
+console.log(parseFloat(myVar).toFixed(2)); // 100.57
+
+
+let num = 10;
+
+console.log(Number.isInteger(num)+1); // 2
+
+
+let a="Elzero Web School";
+console.log (a.charAt(2).toUpperCase()+a.slice(3,6));
+
+console.log(a.charAt(13).toUpperCase().repeat(8))
+
+ console.log(a.split(" ",1));
+
+ console.log(`${a.substr(0,6)} ${a.substr(11)}`)
+
+ console.log(`${a.charAt(0).toLocaleLowerCase()}${a.substring(1,a.length-1).toUpperCase()}${a.charAt(a.length-1)}`) */
+
+
+
+/*  let userName = "Elzero";
+console.log(userName.charAt(0).toLocaleLowerCase()); // e
+console.log(userName.substring(0,1)); // e
+console.log(userName.substr(0,1)); // e
+console.log(userName.slice(0,1)); // e
+console.log(userName.split("",1)); // e
+console.log(userName.at(0).toLocaleLowerCase().repeat(3)); // eee */
+
+/* let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toLocaleUpperCase())); // True
+console.log(word.endsWith(letterO.toLocaleLowerCase())); // True */
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 && 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(typeof -50 === typeof +"-40"); // true
+console.log(typeof 10 == typeof -"-40"); // true
+console.log(typeof "10" !== typeof 10); // true
+console.log(! 20 == false); // true
