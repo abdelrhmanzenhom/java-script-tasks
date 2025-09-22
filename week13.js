a40122d99dee05f00bc5a2698706973965e35260
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-09-22 17:58:06 +0300

    for merge

--- week13.js
+++ week13.js
@@ -1,4 +1,4 @@
-let classesToAdd=document.querySelector(".classes-to-add");
+/* let classesToAdd=document.querySelector(".classes-to-add");
 let classesToRemove=document.querySelector(".classes-to-remove");
 let current=document.querySelector("[title='Current']");
 let printClasses=document.querySelector(".classes-list div")
@@ -23,8 +23,9 @@ function printt(){
     console.log(current.classList[i]);
     printClasses.appendChild(span);
    }
+   if(!printClasses.hasChildNodes())printClasses.innerHTML="No Classes To Show";
 }
-
+window.onload=printt();
 classesToRemove.onblur=function(){
      let classArr=classesToRemove.value.split(" ");
      if(classArr.length!=0&&classArr[0]!="")
@@ -35,4 +36,22 @@ classesToRemove.onblur=function(){
     console.log(current.classList);
     printt();
     classesToRemove.value="";
-}
+} */
+/* div=document.querySelector(".our-element");
+p=document.querySelector("p");
+document.body.removeChild(p);
+divafter=document.createElement("div")
+divbefor=document.createElement("div");
+divbefor.classList.add("start");
+divbefor.innerHTML="Start"
+console.log(divafter)
+divafter.classList.add("end")
+divafter.innerHTML="End"
+divafter.title="End Element";
+divafter.setAttribute("data-value","Start");
+div.after(divafter);
+div.before(divbefor); */
+
+/* let elzero=document.querySelector("div").lastChild.textContent.trim();
+console.log(elzero) */
+
